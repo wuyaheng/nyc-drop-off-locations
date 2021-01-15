ce1c8567383693a5e76ebceccd5c9b37b90ac18d
wuyaheng <wuyaheng2016@gmail.com>
2021-01-14 21:20:35 -0500

    show different drop off locations

--- src/components/MapBox/Map.js
+++ src/components/MapBox/Map.js
@@ -24,7 +24,57 @@ export default (props) => {
           accessToken: process.env.REACT_APP_MAP_API_KEY,
         }
       ).addTo(mymap);
-      props.results.textile.forEach((pin) => L.marker([pin.latitude, pin.longitude]).addTo(mymap).bindTooltip("<p>" + pin.ntaname + "</p>"));
+
+      var blueIcon = new L.Icon({ 
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      var redIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      var goldIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      var orangeIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41] 
+      });
+
+      props.results.textile.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: blueIcon}).addTo(mymap).bindTooltip("<p>" + pin.vendor_name + "</p>"));
+      props.results.foodScrap.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: redIcon}).addTo(mymap).bindTooltip("<p>" + pin.food_scrap_drop_off_site + "</p>"));
+      props.results.electronics.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: goldIcon}).addTo(mymap).bindTooltip("<p>" + pin.dropoff_sitename + "</p>"));
+      props.results.pharmaceutical.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: orangeIcon}).addTo(mymap).bindTooltip("<p>" + pin.drop_off_site_name + "</p>")); 
+      props.results.leaf.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: greenIcon}).addTo(mymap).bindTooltip("<p>" + pin.site_name + "</p>")); 
     }
 
     return () => (MAP_CONTAINER.innerHTML = "");
