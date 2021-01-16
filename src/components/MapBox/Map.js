ce0483849d96137bf7946a2440ded1024af1e5f5
wuyaheng <wuyaheng2016@gmail.com>
2021-01-16 13:34:53 -0500

    add tooltip for textile

--- src/components/MapBox/Map.js
+++ src/components/MapBox/Map.js
@@ -10,7 +10,7 @@ export default (props) => {
       MAP_ID.setAttribute("id", "mapid");
       MAP_CONTAINER.appendChild(MAP_ID);
 
-      const mymap = L.map("mapid").setView([props.lat, props.lon], 16);
+      const mymap = L.map("mapid").setView([props.lat, props.lon], 14);
 
       L.tileLayer(
         "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -18,7 +18,7 @@ export default (props) => {
           attribution:
             'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
           maxZoom: 18,
-          id: "mapbox/streets-v11",
+          id: "mapbox/streets-v8",
           tileSize: 512,
           zoomOffset: -1,
           accessToken: process.env.REACT_APP_MAP_API_KEY,
@@ -70,15 +70,15 @@ export default (props) => {
         shadowSize: [41, 41] 
       });
 
-      props.results.textile.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: blueIcon}).addTo(mymap).bindTooltip("<p>" + pin.vendor_name + "</p>"));
-      props.results.foodScrap.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: redIcon}).addTo(mymap).bindTooltip("<p>" + pin.food_scrap_drop_off_site + "</p>"));
-      props.results.electronics.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: goldIcon}).addTo(mymap).bindTooltip("<p>" + pin.dropoff_sitename + "</p>"));
-      props.results.pharmaceutical.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: orangeIcon}).addTo(mymap).bindTooltip("<p>" + pin.drop_off_site_name + "</p>")); 
-      props.results.leaf.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: greenIcon}).addTo(mymap).bindTooltip("<p>" + pin.site_name + "</p>")); 
+      props.results.textile.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: blueIcon}).addTo(mymap).bindTooltip("<h6>Textile Drop-Off Location</h6><p><b>Name:</b> " + pin.vendor_name + "</p><p><b>Items Accepted:</b> " + pin.items_accepted + "</p><p><b>Phone:</b> " + pin.public_phone + "</p><p><b>Email:</b> " + pin.public_email + "</p><p><b>Address:</b> " + pin.address + ", New York, NY" + pin.zipcode + "</p>"));
+      props.results.foodScrap.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: redIcon}).addTo(mymap).bindTooltip("<h6>Food Scrap Drop-Off Location</h6><p>" + pin.food_scrap_drop_off_site + "</p>"));
+      props.results.electronics.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: goldIcon}).addTo(mymap).bindTooltip("<h6>Electronics Drop-Off Location</h6><p>" + pin.dropoff_sitename + "</p>"));
+      props.results.pharmaceutical.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: orangeIcon}).addTo(mymap).bindTooltip("<h6>Pharmaceutical Drop-Off Location</h6><p>" + pin.drop_off_site_name + "</p>")); 
+      props.results.leaf.forEach((pin) => L.marker([pin.latitude, pin.longitude],{icon: greenIcon}).addTo(mymap).bindTooltip("<h6>Leaf Drop-Off Location</h6><p>" + pin.site_name + "</p>")); 
     }
 
     return () => (MAP_CONTAINER.innerHTML = "");
   }, [props.lat, props.lon, props.pins]);
 
   return <div id="map-container"></div>; 
-};
+}
